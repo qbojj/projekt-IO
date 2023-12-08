274580b7beb9cf9af4db76180e7afabeadf5207f
MaksWisniewski <makswisnie@gmail.com>
2023-12-08 18:23:50 +0100

    cleanup

---
+++ KoncepcjaWykonaniaSystemu/a.sh
@@ -0,0 +1,10 @@
+# rm presentation.pdf
+
+# pandoc -t beamer './Koncepcja wykonania systemu.md' -V theme:Warsaw -o presentation.pdf
+#
+#
+pandoc  './Koncepcja wykonania systemu.md'  -o Koncepcja wykonania systemu.pdf
+
+
+
+pandoc './SpecyfikacjaWymagan.md' -o SpecyfikacjaWymagan.pdf
